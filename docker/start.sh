dddccb9115a95cb9458791d0ce661a76150a54a4
ignwombat <ignwombat@gmail.com>
2023-04-28 17:34:42 +0200

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#v=$(LC_COLLATE=C; /usr/bin/printf '%s\c' */)
-./jre/bin/java -Xms${MIN_RAM} -Xmx${MAX_RAM} -jar server.jar nogui
+v=$(LC_COLLATE=C; /usr/bin/printf '%s\c' ./jre)
+echo $v
+./jre&${v}bin/java -Xms${MIN_RAM} -Xmx${MAX_RAM} -jar server.jar nogui
