2d50ecb6444f46f72f9c463895df54e8874aaa1c
ignwombat <ignwombat@gmail.com>
2023-04-28 17:31:27 +0200

    Fix?

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-v=$(LC_COLLATE=C; /usr/bin/printf '%s\c' */)
-./jre/${v}/bin/java -Xms${MIN_RAM} -Xmx${MAX_RAM} -jar server.jar nogui
+#v=$(LC_COLLATE=C; /usr/bin/printf '%s\c' */)
+./jre/bin/java -Xms${MIN_RAM} -Xmx${MAX_RAM} -jar server.jar nogui
